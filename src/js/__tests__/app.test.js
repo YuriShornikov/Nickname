4c96f66370de3e297a493bc7c1a546d35e99a8ea
Yuri Shornikov <dbonlyfun@gmail.com>
2024-02-11 22:49:28 +0300

    correct formuls and add tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,10 +2,28 @@ import Validator from "../app";
 
 describe('regularly', () => {
     const reg = new Validator();
-    test('correct rules', () => {
-        expect(reg.validateUsername('Pokemon-111')).toBe(true);
+    test('All agreed the symbols', () => {
+        expect(reg.validateUsername('Pokemon-111_a')).toBe(true);
+    })
+    test('russian symbol - false', () => {
+        expect(reg.validateUsername('йPokemon-111')).toBe(false);
     })
     test('uncorrect rules', () => {
         expect(reg.validateUsername('Pokemon-1111')).toBe(false);
+    })
+    test('1 at the beginning', () => {
+        expect(reg.validateUsername('1Pokemon-1111')).toBe(false);
+    })
+    test('_ at the beginning', () => {
+        expect(reg.validateUsername('_Pokemon-1111')).toBe(false);
+    })
+    test('_ in the and', () => {
+        expect(reg.validateUsername('Pokemon-1111_')).toBe(false);
+    })
+    test('- in the and', () => {
+        expect(reg.validateUsername('Pokemon-1111-')).toBe(false);
+    })
+    test('- at the beginning', () => {
+        expect(reg.validateUsername('-Pokemon-1111')).toBe(false);
     })
 })
--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,10 @@ export default class Validator {
 
         const usernameRegex = /^(?![_-])[\w-]*[a-zA-Z]+[\w-]*$/;
         const digitsRegex = /\d{4,}/;
+        const startEndRegex = /^[_-]*[a-zA-Z]+.*[a-zA-Z]+[_-]*$/;
 
         // Проверяем на несоответствие
-        if (digitsRegex.test(username) || !usernameRegex.test(username)) {
+        if (digitsRegex.test(username) || !usernameRegex.test(username) || !startEndRegex.test(username)) {
             return false;
         }
 
